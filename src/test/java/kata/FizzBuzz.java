eb1b475f7f194d79d8c66626f2dfe510cba99d6a
Francisco San Emeterio <francisco.sanemeterio@bts.tech>
2017-08-06 21:12:50 -0500

    Test for multiples of three with green result

--- src/test/java/kata/FizzBuzz.java
+++ src/test/java/kata/FizzBuzz.java
@@ -6,24 +6,30 @@ import java.util.List;
 public class FizzBuzz {
 
 	//Creating an internal field so it can be read in another method
-	private List<Integer> intList;
+	private List<String> resultList;
 	
 	//Main method to create the list
 	public void run() {
-		intList = new ArrayList<Integer>();
+		resultList = new ArrayList<String>();
 		for (int i=1; i<=100; i++) {
-			intList.add(i);
+			boolean multipleOfThree = i%3==0;
+			if (multipleOfThree) {
+				resultList.add("Fizz");
+			} else {
+				resultList.add(String.valueOf(i));
+			}
 		}
 		
 	}
 
 	//Method to get the total size of the Array
 	public int getNumberLength() {
-		return intList.size();
+		return resultList.size();
 	}
 
-	public Integer getValueForPosition(int index) {
-		return intList.get(index);
+	public String getValueForPosition(int position) {
+		//Position starts on 1 while index starts on 0
+		return resultList.get(position-1);
 	}
 
 }
